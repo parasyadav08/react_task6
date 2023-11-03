d57455cca4d598d8ad6bcf560c29506bf9334906
Paras Yadav <114516630+parasyadav08@users.noreply.github.com>
2023-11-03 12:11:49 +0530

    Add files via upload

---
+++ REACT_TASK_6/src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <header>
+      <h1>Paras Yadav</h1>
+      <p>Web Developer</p>
+    </header>
+  );
+}
+
+export default Header;
